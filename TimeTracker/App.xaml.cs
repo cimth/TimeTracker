9529be488a31a22932127a8ac1f3e2bf715f6854
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2022-12-31 16:36:15 +0100

    Use the current culture in WPF
    The current culture assures that e.g. date and time formats are represented as the User expects them.

--- TimeTracker/App.xaml.cs
+++ TimeTracker/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace TimeTracker
 {
@@ -13,5 +15,23 @@ namespace TimeTracker
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            this.UseCurrentCultureInWpf();
+        }
+
+        /**
+         * Uses the current culture for WPF so that e.g. date and time formats are represented as the User
+         * expects them.
+         * 
+         * See:
+         * https://stackoverflow.com/questions/1062160/wpf-xaml-stringformat-datetime-output-in-wrong-culture
+         */
+        private void UseCurrentCultureInWpf()
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));   
+        }
     }
 }
