cfaa72c25ffb15e4abfb20d1ee727118f9a17d27
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2023-01-03 16:06:17 +0100

    Change between the Entries and the Categories View in the main window

--- TimeTracker/ViewModels/MainWindowViewModel.cs
+++ TimeTracker/ViewModels/MainWindowViewModel.cs
@@ -2,27 +2,40 @@
 using TimeTracker.Dialog;
 using TimeTracker.Models.Database;
 using TimeTracker.Models.Services;
+using TimeTracker.Utils;
 using TimeTracker.ViewModels.Command;
 using TimeTracker.ViewModels.CreateUpdate;
 using TimeTracker.ViewModels.Read;
 
 namespace TimeTracker.ViewModels;
 
-public class MainWindowViewModel
+public class MainWindowViewModel : NotifyPropertyChangedImpl
 {
     // ==============
     // Properties
     // ==============
-    
-   public ReadCategoriesViewModel ReadCategoriesViewModel { get; }
-   public ReadEntriesViewModel ReadEntriesViewModel { get; }
+
+    public bool IsCategoriesViewShown
+    {
+        get => this._isCategoriesViewShown;
+        set => SetField(ref this._isCategoriesViewShown, value);
+    }
+
+    public bool IsEntriesViewShown
+    {
+        get => this._isEntriesViewShown;
+        set => SetField(ref this._isEntriesViewShown, value);
+    }
+
+    public ReadCategoriesViewModel ReadCategoriesViewModel { get; }
+    public ReadEntriesViewModel ReadEntriesViewModel { get; }
     
     // ==============
     // Commands
     // ==============
 
-    public ICommand ShowCreateUpdateEntryDialogCommand { get; }
-    public ICommand ShowCreateUpdateCategoryDialogCommand { get; }
+    public ICommand ShowCategoriesViewCommand { get; }
+    public ICommand ShowEntriesViewCommand { get; }
     
     // ==============
     // Fields
@@ -32,6 +45,9 @@ public class MainWindowViewModel
     private readonly CreateUpdateCategoryViewModel _createUpdateCategoryViewModel;
 
     private readonly DialogService _dialogService;
+    
+    private bool _isCategoriesViewShown;
+    private bool _isEntriesViewShown;
 
     // ==============
     // Initialization
@@ -54,5 +70,29 @@ public class MainWindowViewModel
         
         this.ReadCategoriesViewModel = new ReadCategoriesViewModel(categoryService, this._dialogService, this._createUpdateCategoryViewModel);
         this.ReadEntriesViewModel = new ReadEntriesViewModel(entryService, this._dialogService, this._createUpdateEntryViewModel);
+        
+        // Initialize the commands.
+        this.ShowCategoriesViewCommand = new DelegateCommand(this.ShowCategoriesView);
+        this.ShowEntriesViewCommand = new DelegateCommand(this.ShowEntriesView);
+        
+        // Initialize the main view that is shown on startup.
+        this.IsCategoriesViewShown = false;
+        this.IsEntriesViewShown = true;
+    }
+    
+    // ==============
+    // Command actions
+    // ==============
+
+    public void ShowCategoriesView()
+    {
+        this.IsCategoriesViewShown = true;
+        this.IsEntriesViewShown = false;
+    }
+
+    public void ShowEntriesView()
+    {
+        this.IsCategoriesViewShown = false;
+        this.IsEntriesViewShown = true;
     }
 }
