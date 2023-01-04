37a39f6e5531aadbdc9ca93388c317b6de6cab4a
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2023-01-04 16:32:56 +0100

    Delete a Category via the GUI
    - Ask the user for confirmation before deleting.
    
    - Only delete the Category if it is not used by any Entry.
    
    - Show a success or error dialog after processing.

--- TimeTracker/ViewModels/Read/ReadCategoriesViewModel.cs
+++ TimeTracker/ViewModels/Read/ReadCategoriesViewModel.cs
@@ -1,13 +1,12 @@
-using System;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using TimeTracker.Dialog;
-using TimeTracker.Models;
 using TimeTracker.Models.Entities;
 using TimeTracker.Models.Services;
 using TimeTracker.Utils;
 using TimeTracker.ViewModels.Command;
 using TimeTracker.ViewModels.CreateUpdate;
+using TimeTracker.ViewModels.Dialog;
 
 namespace TimeTracker.ViewModels.Read;
 
@@ -27,6 +26,7 @@ public class ReadCategoriesViewModel
     
     public ICommand CreateCommand { get; }
     public ICommand UpdateCommand { get; }
+    public ICommand DeleteCommand { get; }
     
     // ==============
     // Fields
@@ -50,6 +50,7 @@ public class ReadCategoriesViewModel
 
         this.CreateCommand = new DelegateCommand(this.Create);
         this.UpdateCommand = new DelegateCommand(this.Update);
+        this.DeleteCommand = new DelegateCommand(this.Delete);
 
         this.Categories = this._categoryService.Categories;
     }
@@ -68,4 +69,34 @@ public class ReadCategoriesViewModel
         this._dialogService.ShowCreateUpdateCategoryDialog(this._createUpdateCategoryViewModel, this.SelectedCategory);
     }
 
+    private void Delete()
+    {
+        // Only continue if a category is selected.
+        if (this.SelectedCategory == null)
+        {
+            return;
+        }
+
+        // Ask the user for confirmation.
+        ConfirmDialogViewModel confirmViewModel = new ConfirmDialogViewModel("Should the category really be deleted?");
+        bool? result = this._dialogService.ShowConfirmDialog(confirmViewModel);
+
+        // Remove the category if the user confirmed.
+        if (result != null && result.Value)
+        {
+            bool success = this._categoryService.Delete(this.SelectedCategory);
+            
+            // Show success / error dialog.
+            if (success)
+            {
+                MessageDialogViewModel messageViewModel = new MessageDialogViewModel("Success", "The category was successfully deleted.");
+                this._dialogService.ShowMessageDialog(messageViewModel);
+            }
+            else
+            {
+                MessageDialogViewModel messageViewModel = new MessageDialogViewModel("Error", "The category cannot be deleted because it is used in at least one Entry.");
+                this._dialogService.ShowMessageDialog(messageViewModel);
+            }
+        }
+    }
 }
